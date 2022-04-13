d6958810b66cf5b477ebc30d9461ef3a5ed8e048
mihaicCrafting <mihai.cira@craftingsoftware.com>
2022-04-13 08:48:39 +0300

    tech scroll event

--- src/Components/HomePage/Tech/Tech.js
+++ src/Components/HomePage/Tech/Tech.js
@@ -1,10 +1,32 @@
 import './Tech.scss'
-import React, {useRef} from 'react'
+import React, {useRef,useEffect} from 'react'
 import {Stack} from "@mui/material";
 
 export default function Tech() {
     const using = useRef()
     const pic = useRef()
+
+    useEffect(() => {
+        document.addEventListener("scroll", scrollEvent);
+      }, []);
+    
+      const scrollEvent = () => {
+        if (window.innerWidth < 750) return;
+    
+        const Y = window.scrollY;
+    
+        let transformValue = Y / 80;
+    
+        using.current.style.transform = `translateX(-${transformValue}%)`;
+
+        transformValue = Y / 30;
+    
+        pic.current.style.transform = `translateX(${transformValue}%)`;
+    
+        // responsive.current.firstChild.style.transform = `scale()`
+    
+        console.log(Y);
+      };
 
   return (
     <div className="section" id="tech-section">

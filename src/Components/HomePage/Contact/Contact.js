4e6efc4919abc60ebf6fea24399d0230407d894c
mihaicCrafting <mihai.cira@craftingsoftware.com>
2022-04-18 07:43:49 +0300

    form

--- src/Components/HomePage/Contact/Contact.js
+++ src/Components/HomePage/Contact/Contact.js
@@ -1,51 +1,103 @@
-import React from 'react'
-import "./Contact.scss"
-import Divider from "../../../assets/svg/contact-divider.svg"
-import ThumbsUp from "../../../assets/svg/thumbs_up.svg"
-import {Stack} from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef, useState } from 'react';
+import './Contact.scss';
+import Divider from '../../../assets/svg/contact-divider.svg';
+import ThumbsUp from '../../../assets/svg/thumbs_up.svg';
+import { Stack, Alert } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
-import emailjs from "emailjs-com";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const from = useRef();
+  const text = useRef();
+  const [alert, setAlert] = useState(false);
 
-    const sendEmail = () =>{
-        emailjs.send("service_c0zrve8","template_c6iu695",{
-            email: "email",
-            message: "message",
-            date: "date.toString()"
-        },'Mpm1e_o9P8YhwFyNt');
-        // },'user_T4jIQ0R5325FA0lZ8gMtO');
+  const sendEmail = () => {
+    const fromTxt = from.current.value;
+    const textTxt = text.current.value;
 
-        //Send emailx
-        console.log("email sent");
-
-        
+    if (textTxt == '') {
+      setAlert(true);
+      return;
     }
+
+    console.log(textTxt);
+
+    return;
+
+    const date = new Date();
+    emailjs.send(
+      'service_c0zrve8',
+      'template_c6iu695',
+      {
+        from: 'email',
+        text: 'message',
+        date: date.toString(),
+      },
+      'Mpm1e_o9P8YhwFyNt'
+    );
+    console.log('email sent');
+  };
+
   return (
     <div className="section" id="contact-section">
-        <img id="divider" src={Divider}/>
+      <img id="divider" src={Divider} />
 
-        <Stack alignItems="center" justifyContent="center" direction="column" sx={{ marginBottom: "5rem"}}>
-            <h1>contact</h1>
-            <a href="">ciramihai291@gmail.com</a>
-        </Stack>
+      {alert && (
+        <WarningAlert
+          close={() => {
+            setAlert(false);
+          }}
+        />
+      )}
 
-        <Stack alignItems="baseline" justifyContent="center" direction="column" className="form" gap="1rem" sx={{marginBottom: "5rem"}}>
-            <input placeholder='from'/>
-            <textarea placeholder='message'/>
-            <button onClick={sendEmail}>submit</button>
-        </Stack>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        sx={{ marginBottom: '5rem' }}
+      >
+        <h1>contact</h1>
+        <a href="">ciramihai291@gmail.com</a>
+      </Stack>
 
-        <Stack alignItems="center" justifyContent="center" direction="column" className="uglybtn" gap="1rem">
-            <h1>or...</h1>
-            <p>hit this ugly like button if you like this website <FontAwesomeIcon icon={faFaceSmile}/></p>
-            <button >
-                <img src={ThumbsUp}/>
-            </button>
-        </Stack>
-        
+      <Stack
+        alignItems="baseline"
+        justifyContent="center"
+        direction="column"
+        className="form"
+        gap="1rem"
+        sx={{ marginBottom: '5rem' }}
+      >
+        <input placeholder="from" ref={from} />
+        <textarea placeholder="message" ref={text} />
+        <button onClick={sendEmail}>submit</button>
+      </Stack>
 
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        className="uglybtn"
+        gap="1rem"
+      >
+        <h1>or...</h1>
+        <p>
+          hit this ugly like button if you like this website{' '}
+          <FontAwesomeIcon icon={faFaceSmile} />
+        </p>
+        <button>
+          <img src={ThumbsUp} />
+        </button>
+      </Stack>
     </div>
-  )
+  );
+}
+
+function WarningAlert(props) {
+  return (
+    <Alert severity="warning" className="alertBox" onClose={props.close}>
+      This is a warning alert — check it out!
+    </Alert>
+  );
 }
--- src/Components/HomePage/Tech/Tech.js
+++ src/Components/HomePage/Tech/Tech.js
@@ -1,85 +1,87 @@
-import './Tech.scss'
-import {useRef,useEffect} from 'react'
-import {Stack} from "@mui/material";
+import './Tech.scss';
+import { useRef, useEffect } from 'react';
+import { Stack } from '@mui/material';
 import ReactSvg from './ReactSvg/ReactSvg';
-import Firebase from "../../../assets/img/firebase.png";
-import Nodejs from "../../../assets/img/nodejs.png";
-import Postgresql from "../../../assets/img/postgresql.png";
-import Threejs from "../../../assets/img/threejs.png";
-import Phoenix from "../../../assets/img/phoenix.png";
-import Materialui from "../../../assets/img/materialui.png";
-import Jquery from "../../../assets/img/jquery.png";
-import Mongodb from "../../../assets/img/mongodb.png";
+import Firebase from '../../../assets/img/firebase.png';
+import Nodejs from '../../../assets/img/nodejs.png';
+import Postgresql from '../../../assets/img/postgresql.png';
+import Threejs from '../../../assets/img/threejs.png';
+import Phoenix from '../../../assets/img/phoenix.png';
+import Materialui from '../../../assets/img/materialui.png';
+import Jquery from '../../../assets/img/jquery.png';
+import Mongodb from '../../../assets/img/mongodb.png';
 
 export default function Tech() {
-    const using = useRef()
-    const pic = useRef()
+  const using = useRef();
+  const pic = useRef();
 
-    useEffect(() => {
-        document.addEventListener("scroll", scrollEvent);
-      }, []);
-    
-      const scrollEvent = () => {
-        if (window.innerWidth < 750) return;
-    
-        const Y = window.scrollY;
-    
-        let transformValue = Y / 80;
-    
-        using.current.style.transform = `translateX(-${transformValue}%)`;
+  useEffect(() => {
+    document.addEventListener('scroll', scrollEvent);
+  }, []);
 
-        transformValue = Y / 30;
-    
-        pic.current.style.transform = `translateX(${transformValue}%)`;
-      };
+  const scrollEvent = () => {
+    if (window.innerWidth < 750) return;
+
+    const Y = window.scrollY;
+
+    let transformValue = Y / 30 - 150;
+
+    using.current.style.transform = `translateX(-${transformValue}%)`;
+
+    transformValue = Y / 30;
+
+    // pic.current.style.transform = `translateX(${transformValue}%)`;
+  };
 
   return (
     <div className="section" id="tech-section">
-        <Stack direction="column" alignItems="center" justifyContent="center">
-            <h2 ref={using}>using...</h2>
-            <div ref={pic}>
-                <ReactSvg/>
-            </div>
-        </Stack>
+      <Stack direction="column" alignItems="center" justifyContent="center">
+        <h2 ref={using}>using...</h2>
+        <div ref={pic}>
+          <ReactSvg />
+        </div>
+        <h6 style={{ marginTop: '4rem' }}>... and</h6>
+      </Stack>
 
-        <Stack direction="row" flexWrap="wrap" gap="4rem" alignItems="center" justifyContent="center">
+      <Stack
+        direction="row"
+        flexWrap="wrap"
+        gap="4rem"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <a href="https://firebase.google.com/" target="_blank">
+          <img src={Firebase} />
+        </a>
 
-            <a href="">
-                <img src={Firebase}/>
-            </a>
+        <a href="https://nodejs.org/en/" target="_blank">
+          <img src={Nodejs} />
+        </a>
 
-            <a href="">
-                <img src={Nodejs}/>
-            </a>
+        <a href="https://www.postgresql.org/" target="_blank">
+          <img src={Postgresql} />
+        </a>
 
-            <a href="">
-                <img src={Postgresql}/>
-            </a>
+        <a href="https://www.mongodb.com/" target="_blank">
+          <img src={Mongodb} />
+        </a>
 
-            <a href="">
-                <img src={Mongodb}/>
-            </a>
+        <a href="https://mui.com/" target="_blank">
+          <img src={Materialui} />
+        </a>
 
-            <a href="">
-                <img src={Materialui}/>
-            </a>
+        <a href="https://jquery.com/" target="_blank">
+          <img src={Jquery} />
+        </a>
 
-            <a href="">
-                <img src={Jquery}/>
-            </a>
+        <a href="https://phoenixframework.org/" target="_blank">
+          <img src={Phoenix} />
+        </a>
 
-
-            <a href="">
-                <img src={Phoenix}/>
-            </a>
-
-            <a href="">
-                <img src={Threejs}/>
-            </a>
-
-
-        </Stack>
-        
+        <a href="https://threejs.org/" target="_blank">
+          <img src={Threejs} />
+        </a>
+      </Stack>
     </div>
-  )
+  );
 }
